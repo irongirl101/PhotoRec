9d6f3b9283a62b64676210524b88a02ef3a84ede
irongirl101 <141424562+irongirl101@users.noreply.github.com>
2025-08-19 12:50:25 +0530

    testing

--- VideoCapture.py
+++ VideoCapture.py
@@ -1,15 +1,15 @@
 import cv2 as c 
 
-cap = c.VideoCapture(0)
+cap = c.VideoCapture(0)#0-> 1 webcam, 1-> 2 webcams etc etc 
 
-while True: 
-    ret,frame = cap.read()
+while True: #loop for frames 
+    ret,frame = cap.read() # ret is a bool as to whether a frame was read successfully or not , frame is a numpy array ffo the image captured 
     if not ret: 
         break
-    c.imshow("image",frame)
+    c.imshow("image",frame) # shows frame 
 
-    if c.waitKey(0) and 0xFF == "q": 
+    if c.waitKey(1) and 0xFF == "q":  #wait till a key is pressed in order to exit 
         break
 
-cap.release()
-c.destroyAllWindows()
+cap.release() #close the camera 
+c.destroyAllWindows() #close the windows 
